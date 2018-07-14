4b372650e70fb9a4401fe42bf6cd6d5dae6a3b59
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-07-15 05:09:46 +0900

    Front apage HTML now generated from a template

--- functional_tests.py
+++ functional_tests.py
@@ -1,11 +1,15 @@
 #!/usr/bin/env python
 # -*- coding:utf-8 -*-
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+
+import time
 import unittest
+
 
 class NewVisitorTest(unittest.TestCase):
     def setUp(self):
-        self.browser =  webdriver.Chrome()
+        self.browser = webdriver.Chrome()
 
     def tearDown(self):
         self.browser.quit()
@@ -18,18 +22,22 @@ class NewVisitorTest(unittest.TestCase):
         self.assertIn('To-Do', header_text)
 
         inputbox = self.browser.find_element_by_id('id-new-item')
-        self.assertEqual(inputbox.get_attribute('placeholder'), 'Enter a to-do item')
+        self.assertEqual(
+            inputbox.get_attribute('placeholder'), 'Enter a to-do item')
 
         inputbox.send_keys('Budy peacok feathers')
         inputbox.send_keys(Keys.ENTER)
 
         time.sleep(1)
         table = self.browser.find_element_by_id('id-list-table')
-        rows = table.find_element_by_tag_name('tr')
+        rows = table.find_elements_by_tag_name('tr')
 
-        self.assertTrue(any(row.text=='1: Buy peacock feathers' for row in rows))
+        self.assertTrue(
+            any(row.text == '1: Buy peacock feathers' for row in rows),
+            "New to-do item did not appear in table")
 
         self.fail('Finish the test!')
+
 
 if __name__ == '__main__':
     unittest.main(warnings='ignore')
