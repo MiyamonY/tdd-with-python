250d5ed61bc56e1cc2ba0689f342339bee096569
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-07-26 01:00:39 +0900

    Move Fts into their own indivisual files

---
+++ functional_tests/test_list_item_validation.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from .base import FunctionalTest
+from unittest import skip
+
+
+class ItemValidationTest(FunctionalTest):
+    @skip
+    def test_cannot_add_empty_list_items(self):
+        self.fail('write me!')
