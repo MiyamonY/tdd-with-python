6176732134494333045c565a7d56045d85eb00db
MIYAMOTO Yohei <y.miyamoto324@gmail.com>
2018-07-28 16:08:48 +0900

    rename all item input ids and names. still broken

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -7,14 +7,14 @@ from selenium.webdriver.common.keys import Keys
 class ItemValidationTest(FunctionalTest):
     def test_cannot_add_empty_list_items(self):
         self.browser.get(self.live_server_url)
-        self.browser.find_element_by_id('id-new-item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys(Keys.ENTER)
 
         self.wait_for(lambda: self.assertEqual(
             self.browser.find_element_by_css_selector('.has-error').text,
             "You can't have an empty list item"))
 
-        self.browser.find_element_by_id('id-new-item').send_keys('Buy Milk')
-        self.browser.find_element_by_id('id-new-item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Buy Milk')
+        self.get_item_input_box().send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy Milk')
 
         self.browser.find_element_by_id('id-new-item').send_keys(Keys.ENTER)
@@ -22,7 +22,7 @@ class ItemValidationTest(FunctionalTest):
                       self.assertEqual(self.browser.find_element_by_css_selector('.has-error').text,
                                        "You can't have an empty list item"))
 
-        self.browser.find_element_by_id('id-new-item').send_keys('Make tea')
-        self.browser.find_element_by_id('id-new-item').send_keys(Keys.ENTER)
+        self.get_item_input_box().send_keys('Make tea')
+        self.get_item_input_box(j).send_keys(Keys.ENTER)
         self.wait_for_row_in_list_table('1: Buy Milk')
         self.wait_for_row_in_list_table('2: Make tea')
